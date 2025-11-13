8ecf5c73e3982f7ec163226fa02cc26aa39d3b95
bogan <bogang2000@naver.com>
2025-11-13 19:36:01 +0900

    initial commit

---
+++ src/test/java/com/example/TasteMap/TasteMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TasteMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
