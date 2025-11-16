14fc728068a3dbd5b4ebc89344b039a6c0a5fc3b
bogan <bogang2000@naver.com>
2025-11-16 15:51:22 +0900

    refactor : taste-map 경로 허용 보안 설정 추가

--- src/main/java/com/example/TasteMap/config/SecurityConfig.java
+++ src/main/java/com/example/TasteMap/config/SecurityConfig.java
@@ -2,29 +2,29 @@ package com.example.TasteMap.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/v3/api-docs/**",
-                                "/v3/api-docs",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/swagger-ui/index.html",
-                                "/webjars/**"
+                                "/webjars/**",
+                                "/taste-map/**" // taste-map API 허용
                         ).permitAll()
                         .anyRequest().authenticated()
-                )
-                .csrf(csrf -> csrf.disable())
-                .headers(headers -> headers.frameOptions(frame -> frame.disable()));
-
+                );
         return http.build();
     }
 }
