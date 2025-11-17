a9985cf9c96951767bda1b3a295f0dd8e94f63d0
CHO BOGANG <bogang12345@gmail.com>
2025-11-17 21:04:19 +0900

    fix : @Data 어노테이션과 getter/setter 중복 제거

--- src/main/java/com/example/TasteMap/api/dto/image/SearchImageItem.java
+++ src/main/java/com/example/TasteMap/api/dto/image/SearchImageItem.java
@@ -13,44 +13,4 @@ public class SearchImageItem {
     private String thumbnail;
     private String sizeheight;
     private String sizewidth;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public String getThumbnail() {
-        return thumbnail;
-    }
-
-    public void setThumbnail(String thumbnail) {
-        this.thumbnail = thumbnail;
-    }
-
-    public String getSizeheight() {
-        return sizeheight;
-    }
-
-    public void setSizeheight(String sizeheight) {
-        this.sizeheight = sizeheight;
-    }
-
-    public String getSizewidth() {
-        return sizewidth;
-    }
-
-    public void setSizewidth(String sizewidth) {
-        this.sizewidth = sizewidth;
-    }
 }
