b196282aa7c7230e47f43335041a4271178f0506
CHO BOGANG <bogang12345@gmail.com>
2025-11-30 23:28:13 +0900

    refactor : MySQL 연동하여 위시리스트 DB저장 기능 추가

--- src/main/java/com/example/TasteMap/domain/TasteMapEntity.java
+++ src/main/java/com/example/TasteMap/domain/TasteMapEntity.java
@@ -15,9 +15,26 @@ public class TasteMapEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column
     private String title;
+
+    @Column
     private String category;
+
+    @Column
     private String address;
+
+    @Column
     private String roadAddress;
+
+    @Column
     private String imageLink;
+
+    public TasteMapEntity(String title, String category, String address, String roadAddress, String imageLink) {
+        this.title = title;
+        this.category = category;
+        this.address = address;
+        this.roadAddress = roadAddress;
+        this.imageLink = imageLink;
+    }
 }
